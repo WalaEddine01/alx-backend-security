b018c41860340d2dea362ab3cc7af3fa1c8f5b24
WalaEddine01 <walaaeddine33@gmail.com>
2025-07-18 17:33:29 +0100

    Made changes on 17:33:29

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,21 @@
+from django.utils.timezone import now
+from ip_tracking.models import RequestLog
+from ipware import get_client_ip
+
+class IPLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Extract IP
+        ip, _ = get_client_ip(request)
+        # Log request
+        if ip:
+            RequestLog.objects.create(
+                ip_address=ip,
+                timestamp=now(),
+                path=request.path
+            )
+        # Continue processing
+        response = self.get_response(request)
+        return response
